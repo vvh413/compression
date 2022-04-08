49607020fc14bb65c3eea4834b4280375a93fb72
vvh413 <nnenekit@gmail.com>
2022-04-09 00:39:50 +0300

    compressor wrapper refactoring

--- models/compress/wrapper.py
+++ models/compress/wrapper.py
@@ -3,32 +3,28 @@ from compressai.zoo import bmshj2018_hyperprior
 
 
 class HyperpriorWrapper:
-    def __init__(self, model, type="a"):
-        self.model = model
-        if type == "a":
-            self.compress = self.compress_a
-            self.decompress = self.decompress_a
-        elif type == "s":
-            self.compress = self.compress_s
-            self.decompress = self.decompress_s
+    def __init__(self, type="a", *args, **kwargs):
+        self.model = bmshj2018_hyperprior(*args, **kwargs)
 
-    def compress_a(self, x):
+    def to(self, device):
+        return self.model.to(device)
+
+    def eval(self):
+        return self.model.eval()
+
+    def compress(self, x):
+        out = self.model.compress(x)
+        return out
+
+    def decompress(self, strings, shape):
+        x_hat = self.model.decompress(strings, shape)["x_hat"]
+        return x_hat
+
+    def encode(self, x):
         y = self.model.g_a(x)
         return y
 
-    def compress_s(self, x):
-        out = self.model.compress(x)
-        strings, shape = out["strings"], out["shape"]
-
-        z_hat = self.model.entropy_bottleneck.decompress(strings[1], shape)
-        scales_hat = self.model.h_s(z_hat)
-        indexes = self.model.gaussian_conditional.build_indexes(scales_hat)
-        y_hat = self.model.gaussian_conditional.decompress(
-            strings[0], indexes, z_hat.dtype
-        )
-        return y_hat
-
-    def decompress_a(self, y):
+    def entropy_encode(self, y):
         z = self.model.h_a(torch.abs(y))
         shape = z.size()[-2:]
 
@@ -38,11 +34,17 @@ class HyperpriorWrapper:
         scales_hat = self.model.h_s(z_hat)
         indexes = self.model.gaussian_conditional.build_indexes(scales_hat)
         y_strings = self.model.gaussian_conditional.compress(y, indexes)
-        strings = [y_strings, z_strings]
+        return {"strings": [y_strings, z_strings], "shape": shape}
 
-        x_hat = self.model.decompress(strings, shape)["x_hat"]
-        return x_hat
+    def entropy_decode(self, strings, shape):
+        z_hat = self.model.entropy_bottleneck.decompress(strings[1], shape)
+        scales_hat = self.model.h_s(z_hat)
+        indexes = self.model.gaussian_conditional.build_indexes(scales_hat)
+        y_hat = self.model.gaussian_conditional.decompress(
+            strings[0], indexes, z_hat.dtype
+        )
+        return y_hat
 
-    def decompress_s(self, y_hat):
+    def decode(self, y_hat):
         x_hat = self.model.g_s(y_hat).clamp_(0, 1)
         return x_hat
