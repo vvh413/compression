72c7e8d6affbb3a14add121ffa6c00165bd7f9b0
vvh413 <nnenekit@gmail.com>
2022-04-08 23:20:11 +0300

    new docker container and runner

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-docker run -it \
-     --ipc=host --privileged --rm --device=/dev/kfd --device=/dev/dri --group-add video \
-    -p 8888:8888 -v $(pwd):/work \
-    ml
+container=`docker run -dit \
+    --ipc=host --privileged --rm --device=/dev/kfd --device=/dev/dri --group-add video \
+    -p 8888 -v $(pwd):/work \
+    vvh413/ml`
+echo $container
+docker port $container
+docker exec -it $container sh -c "cd /work && bash"
+
