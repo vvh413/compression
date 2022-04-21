119f010538ae8f9ba001e236bfd8abf4a3849a2e
vvh413 <nnenekit@gmail.com>
2022-04-21 15:46:28 +0300

    encoder in captioning model

--- models/captioning/captioning.py
+++ models/captioning/captioning.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class CaptionNet(nn.Module):
@@ -7,11 +8,25 @@ class CaptionNet(nn.Module):
         self,
         n_tokens=10000,
         emb_size=128,
+        pad_ix=0,
         lstm_units=256,
         cnn_feature_size=2048,
-        pad_ix=0,
+        cnn_in_channels=0,
+        cnn_out_channels=0,
+        pool=-1,
     ):
         super(self.__class__, self).__init__()
+
+        self.initial = (
+            nn.Sequential(
+                nn.Conv2d(cnn_in_channels, cnn_out_channels, kernel_size=3),
+                nn.BatchNorm2d(cnn_out_channels),
+                nn.ReLU()
+            )
+            if cnn_in_channels > 0
+            else nn.Identity()
+        )
+        self.pool = pool
 
         self.feature_to_h0 = nn.Linear(cnn_feature_size, lstm_units)
         self.feature_to_c0 = nn.Linear(cnn_feature_size, lstm_units)
@@ -21,6 +36,13 @@ class CaptionNet(nn.Module):
         self.logits = nn.Linear(lstm_units, n_tokens)
 
     def forward(self, image_vectors, captions_ix):
+        image_vectors = self.initial(image_vectors)
+        if self.pool == 0:
+            image_vectors = F.avg_pool2d(image_vectors, image_vectors.shape[-1])
+        elif self.pool > 0:
+            image_vectors = F.max_pool2d(image_vectors, self.pool)
+        image_vectors = image_vectors.view(image_vectors.shape[0], -1)
+
         initial_cell = self.feature_to_h0(image_vectors)
         initial_hid = self.feature_to_c0(image_vectors)
 
